43f2597efdb503176c2036574153d37a43bc38d1
MohamedRagabAnas <mohamed.ragab@nub.edu.eg>
2021-07-26 19:03:22 +0300

    Partitioning calsses Adding First Commit

--- ProjectSourceCode/src/main/scala/ee/ut/cs/bigdata/watdiv/csv/SingleStatementTablePartition.scala
+++ ProjectSourceCode/src/main/scala/ee/ut/cs/bigdata/watdiv/csv/SingleStatementTablePartition.scala
@@ -16,36 +16,39 @@ object SingleStatementTablePartition {
 
     val spark = SparkSession
       .builder()
-      .appName("RDFBench CSV ST")
+      .appName("Watdiv CSV ST Partitioning")
       .getOrCreate()
 
     import spark.implicits._
-        
+
     val ds=args(0)						// data size
     var partitionType=args(1).toLowerCase	// horizontal, predicate or subject
-   
-   val path=s"hdfs://172.17.77.48:9000/user/hadoop/RDFBench/WATDIV/$ds/ST"
-   
+
+    val path=s"hdfs://172.17.77.48:9000/user/hadoop/RDFBench/WATDIV/$ds/ST"
+
+    println("WatDiv ST Partitioning ")
+
    //read original table
    val RDFDF = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(s"$path/VHDFS/CSV/ST100M.csv").toDF()
 
+    println("ST Table is read!")
 
    //partition and save on HDFS
    if(partitionType == "subject")
    {
-     RDFDF.repartition(84, $"Subject").write.option("header", "true").format("csv").mode(SaveMode.Overwrite).save(s"$path/Subject/CSV/ST100M.csv")
+//     RDFDF.repartition(84, $"Subject").write.option("header", "true").format("csv").mode(SaveMode.Overwrite).save(s"$path/Subject/CSV/ST100M.csv")
      println("CSV ST partitioned and saved! Subject!")
    }
 
    else if (partitionType == "predicate")
    {
-     RDFDF.repartition(84, $"Predicate").write.option("header", "true").format("csv").mode(SaveMode.Overwrite).save(s"$path/Predicate/CSV/ST100M.csv")
+//     RDFDF.repartition(84, $"Predicate").write.option("header", "true").format("csv").mode(SaveMode.Overwrite).save(s"$path/Predicate/CSV/ST100M.csv")
      println("CSV ST partitioned and saved! Predicate!")
    }
 
    else if (partitionType == "horizontal")
    {
-     RDFDF.repartition(84).write.option("header", "true").format("csv").mode(SaveMode.Overwrite).save(s"$path/Horizontal/CSV/ST100M.csv")
+//     RDFDF.repartition(84).write.option("header", "true").format("csv").mode(SaveMode.Overwrite).save(s"$path/Horizontal/CSV/ST100M.csv")
      println("CSV ST partitioned and saved! Horizontal!")
    }
          
