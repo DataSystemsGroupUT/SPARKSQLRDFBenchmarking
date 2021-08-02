02ce056d5992908ac3189e618bfa13b273afe577
MohamedRagabAnas <mohamed.ragab@nub.edu.eg>
2021-08-02 20:01:42 +0300

    Modify Partitoning Script

--- scripts/run_partition.sh
+++ scripts/run_partition.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 BENCHMARK=watdiv
-SCHEMA=SingleStatementTablePartition
+SCHEMA=PropertyTablesPartition
 FORMAT=csv
 DS=100M
-PARTITION=horizontal # subject | horizontal | predicate
+PARTITION=predicate # subject | horizontal | predicate
 
-RDF_BENCHMARK_CLASS="ee.ut.cs.bigdata.$BENCHMARK.$FORMAT.$SCHEMA"
+RDF_BENCHMARK_CLASS="ee.ut.cs.bigdata.$BENCHMARK.partitioning.$FORMAT.$SCHEMA"
 RDF_BENCHMARK_JAR="/home/hadoop/SPARKSQLRDFBenchmarking/ProjectSourceCode/target/scala-2.12/rdfbenchmarkingproject_2.12-0.1.jar"
 
 spark-submit \
   --class $RDF_BENCHMARK_CLASS \
-  --master "spark://172.17.77.48:7077" \
+  --master "yarn" \
   --driver-memory 100G \
   --executor-memory 16G \
   --executor-cores 4 \
