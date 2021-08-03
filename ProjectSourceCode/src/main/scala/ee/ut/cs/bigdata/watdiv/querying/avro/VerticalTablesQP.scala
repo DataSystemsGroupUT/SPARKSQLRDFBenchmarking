0b55233f76a4e67bc3c8a91f2a6b393dddc08e44
MohamedRagabAnas <mohamed.ragab@nub.edu.eg>
2021-08-03 17:30:59 +0300

    Querying Package Start Tests VPQP Code Clean.

--- ProjectSourceCode/src/main/scala/ee/ut/cs/bigdata/watdiv/querying/avro/VerticalTablesQP.scala
+++ ProjectSourceCode/src/main/scala/ee/ut/cs/bigdata/watdiv/querying/avro/VerticalTablesQP.scala
@@ -30,17 +30,15 @@ object VerticalTablesQP {
     println("VP Querying!")
     FileSystem.get(sc.hadoopConfiguration).listStatus(new Path(s"$path/$partitionType/Avro")).foreach {
       x =>
-        println(x.getPath + x.getPath.getName)
-        /*
+//        println(x.getPath + x.getPath.getName)
+
         val vpTable = spark.read.format("avro").load(x.getPath().getName())
         println(vpTable.count())
         vpTable.createOrReplaceTempView(x.getPath().getName().drop(4))
-
-         */
     }
 
 
-    //spark.catalog.listTables("default").show()
+    spark.catalog.listTables("default").show()
 
 
 
