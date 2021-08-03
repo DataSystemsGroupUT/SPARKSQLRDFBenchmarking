8a1a13d60ee0951c87ebbfbd06acdcdab23379e9
MohamedRagabAnas <mohamed.ragab@nub.edu.eg>
2021-08-03 12:02:23 +0300

    minor modifications

--- ProjectSourceCode/src/main/scala/ee/ut/cs/bigdata/watdiv/partitioning/orc/PropertyTablesPartition.scala
+++ ProjectSourceCode/src/main/scala/ee/ut/cs/bigdata/watdiv/partitioning/orc/PropertyTablesPartition.scala
@@ -128,8 +128,6 @@ object PropertyTablesPartition {
       purchasePrice.repartition(84).write.option("header", "true").format("orc").mode(SaveMode.Overwrite).save(path + "Predicate/ORC/purchasePrice.orc")
       purchasePurchaseFor.repartition(84).write.option("header", "true").format("orc").mode(SaveMode.Overwrite).save(path + "Predicate/ORC/purchasePurchaseFor.orc")
 
-
-      /*
       val reviewReviewer = Review_DF.select("review", "reviewer").toDF()
       val reviewRating = Review_DF.select("review", "rating").toDF()
       val reviewText = Review_DF.select("review", "text").toDF()
@@ -288,7 +286,7 @@ object PropertyTablesPartition {
       retailerFaxNumber.repartition(84).write.option("header", "true").format("orc").mode(SaveMode.Overwrite).save(path + "Predicate/ORC/retailerFaxNumber.orc")
       retailerAggregateRating.repartition(84).write.option("header", "true").format("orc").mode(SaveMode.Overwrite).save(path + "Predicate/ORC/retailerAggregateRating.orc")
 
-       */
+
 
 
 
