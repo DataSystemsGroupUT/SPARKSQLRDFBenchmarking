8472e6bfa9eb746dea31b2fa2d8488af8a6e21d7
Mohamed Ragab <39013653+MohamedRagabAnas@users.noreply.github.com>
2021-12-16 14:54:55 +0200

    Update run_split.sh

--- scripts/run_split.sh
+++ scripts/run_split.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DS=250M
+DS=100M
 
 RDF_BENCHMARK_CLASS="ee.ut.cs.bigdata.sp2bench.SplitWPTTable"
 RDF_BENCHMARK_JAR="/home/hadoop/RDFBenchMarking/ProjectSourceCode/target/scala-2.12/rdfbenchmarkingproject_2.12-0.1.jar"
