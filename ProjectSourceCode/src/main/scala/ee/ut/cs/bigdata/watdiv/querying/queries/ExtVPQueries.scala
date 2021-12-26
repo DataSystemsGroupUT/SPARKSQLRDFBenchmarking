ce30161066a6b73948875c87cf07226e30e52914
MohamedRagabAnas <mohamed.ragab@nub.edu.eg>
2021-12-26 18:56:23 +0200

    ExtVP WatDiv Queries for 100M

--- ProjectSourceCode/src/main/scala/ee/ut/cs/bigdata/watdiv/querying/queries/ExtVPQueries.scala
+++ ProjectSourceCode/src/main/scala/ee/ut/cs/bigdata/watdiv/querying/queries/ExtVPQueries.scala
@@ -7,31 +7,31 @@ class ExtVPQueries {
   val C1 =
     """
       |SELECT tab0.v1 AS v1 , tab0.v0 AS v0 , tab6.v7 AS v7 , tab4.v5 AS v5 , tab5.v6 AS v6 , tab3.v4 AS v4 , tab2.v3 AS v3 , tab7.v8 AS v8 , tab1.v2 AS v2
-      |FROM    (SELECT obj AS v1 , subject AS v0
+      |FROM    (SELECT object AS v1 , subject AS v0
       |FROM SS_caption_hasReview
       |) tab0
-      |JOIN    (SELECT subject AS v0 , obj AS v3
+      |JOIN    (SELECT subject AS v0 , object AS v3
       |FROM SS_contentRating_caption
       |) tab2
       |ON(tab0.v0=tab2.v0)
-      |JOIN    (SELECT subject AS v0 , obj AS v2
+      |JOIN    (SELECT subject AS v0 , object AS v2
       |FROM SS_text_caption
       |) tab1
       |ON(tab2.v0=tab1.v0)
-      |JOIN    (SELECT subject AS v0 , obj AS v4
+      |JOIN    (SELECT subject AS v0 , object AS v4
       |FROM SS_hasReview_caption
       |) tab3 ON(tab1.v0=tab3.v0)
-      |JOIN    (SELECT obj AS v5 , subject AS v4
+      |JOIN    (SELECT object AS v5 , subject AS v4
       |FROM SO_title_hasReview) tab4
       |ON(tab3.v4=tab4.v4)
-      |JOIN    (SELECT obj AS v6 , subject AS v4
+      |JOIN    (SELECT object AS v6 , subject AS v4
       |FROM SS_reviewer_title) tab5
       |ON(tab4.v4=tab5.v4)
-      |JOIN    (SELECT subject AS v7 , obj AS v6
+      |JOIN    (SELECT subject AS v7 , object AS v6
       |FROM SS_actor_language
       |) tab6
       |ON(tab5.v6=tab6.v6)
-      |JOIN    (SELECT subject AS v7 , obj AS v8
+      |JOIN    (SELECT subject AS v7 , object AS v8
       |FROM SS_language_actor
       |) tab7
       |ON(tab6.v7=tab7.v7)
