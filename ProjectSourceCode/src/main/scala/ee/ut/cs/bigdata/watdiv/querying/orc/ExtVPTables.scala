0d0d47c44a448b5638ac7d841cc4955f027dfe16
MohamedRagabAnas <mohamed.ragab@nub.edu.eg>
2022-01-06 12:26:00 +0100

    ExtVP classes VHDFS edits

--- ProjectSourceCode/src/main/scala/ee/ut/cs/bigdata/watdiv/querying/orc/ExtVPTables.scala
+++ ProjectSourceCode/src/main/scala/ee/ut/cs/bigdata/watdiv/querying/orc/ExtVPTables.scala
@@ -328,7 +328,7 @@ object ExtVPTables {
     SS_type_text.createOrReplaceTempView("SS_type_text")
 
     //create file to write the query run time results
-    val fos = new FileOutputStream(new File(s"/home/hadoop/RDFBenchMarking/logs2/watdiv/$ds/ORC/ExtVP/VHDFS$ds.txt"),true)
+    val fos = new FileOutputStream(new File(s"/home/hadoop/RDFBenchMarking/logs2/watdiv/$ds/orc/ExtVP/VHDFS$ds.txt"),true)
 
     val queries = List(
       new ExtVPQueries C1, new ExtVPQueries C2, new ExtVPQueries C3,
