c0758342c38bd1c8df18c1a075ce3ab7ff036d1d
MohamedRagabAnas <mohamed.ragab@nub.edu.eg>
2022-03-08 17:06:25 +0200

    WPT WatDiv Queries not Flattened Added

--- ProjectSourceCode/src/main/scala/ee/ut/cs/bigdata/watdiv/querying/parquet/WidePropertyTable.scala
+++ ProjectSourceCode/src/main/scala/ee/ut/cs/bigdata/watdiv/querying/parquet/WidePropertyTable.scala
@@ -39,9 +39,9 @@ object WidePropertyTable {
     //create file to write the query run time results
    // val fos = new FileOutputStream(new File(s"/home/hadoop/RDFBenchMarking/logs/$ds/parquet/ST/$ds$partitionType.txt"), true)
 
-        val queries = List(
-      new WPTQueries c1_prost,
-//      new WPTQueries c2_prost,
+    val queries = List(
+//        new WPTQueries c1_prost,
+      new WPTQueries c2_prost,
 //      new WPTQueries c3_prost,
 //      new WPTQueries f1_prost,
 //      new WPTQueries f2_prost,
